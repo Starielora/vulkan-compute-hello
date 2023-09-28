704af1b457b813a73b5bef49bc13f3b8a8b6a410
Starielora <edyko.patryk@gmail.com>
2023-09-28 22:06:17 +0200

    Finishing touches.

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ int main()
     const auto features = vk::ValidationFeaturesEXT(enabled_features);
     const auto context = vk::raii::Context{};
     const auto instance = vk::raii::Instance(context, { {}, &app_info, layers, extensions, &features });
-    const auto severityFlags = vk::DebugUtilsMessageSeverityFlagsEXT(vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError | vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo | vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose);
-    const auto messageTypeFlags = vk::DebugUtilsMessageTypeFlagsEXT(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
-    const auto debugMessenger = instance.createDebugUtilsMessengerEXT({ {}, severityFlags, messageTypeFlags, &debugCallback });
+    const auto severity_flags = vk::DebugUtilsMessageSeverityFlagsEXT(vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError | vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo | vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose);
+    const auto message_type_flags = vk::DebugUtilsMessageTypeFlagsEXT(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+    const auto debug_messenger = instance.createDebugUtilsMessengerEXT({ {}, severity_flags, message_type_flags, &debugCallback });
     const auto physical_devices = instance.enumeratePhysicalDevices();
     const auto& physical_device = physical_devices.at(0);
 
@@ -39,8 +39,7 @@ int main()
         throw std::runtime_error("Could not find compute queue"); // should not happen because at least one queue must be compute
     }();
 
-    const auto prio = vk::ArrayProxy<const float>{ 1.0f };
-    const auto compute_queue_create_info = vk::DeviceQueueCreateInfo({}, compute_family_index, prio);
+    const auto compute_queue_create_info = vk::DeviceQueueCreateInfo({}, compute_family_index, 1.0f);
 
     const auto logical_device = physical_device.createDevice({ {}, compute_queue_create_info });
     const auto print_shader = std::string(R"(
@@ -58,7 +57,7 @@ int main()
     const auto command_pool = logical_device.createCommandPool({ {}, compute_family_index });
     const auto allocate_info = vk::CommandBufferAllocateInfo(*command_pool, vk::CommandBufferLevel::ePrimary, 1);
     const auto command_buffers = logical_device.allocateCommandBuffers(allocate_info);
-    auto& command_buffer = command_buffers[0];
+    const auto& command_buffer = command_buffers[0];
     command_buffer.begin(vk::CommandBufferBeginInfo{});
     command_buffer.bindPipeline(vk::PipelineBindPoint::eCompute, *pipeline);
     command_buffer.dispatch(8, 1, 1);
